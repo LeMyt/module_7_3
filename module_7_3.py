ce58a21ba0d7f15f500dfcc2804f612dc8851bfe
LeMyt <72269801+LeMyt@users.noreply.github.com>
2024-10-16 17:51:00 +0500

    Add files via upload

---
+++ module_7_3.py
@@ -0,0 +1,27 @@
+import io
+
+class WordsFinder:
+    def __init__(self, *file_names):
+        self.file_names = file_names
+        #print(self.file_names)
+
+    def get_all_words(self):
+        all_words = {}
+        line3 = []
+        for i in self.file_names:
+            #print(i)
+            with open(i, encoding='utf-8') as file:
+                for line in file:
+                    for line2 in line.lower().split():
+                        #print(line2)
+                        line3.append(line2)
+                        #line3.append(line2.split('\n'))
+            #print(line3)
+            all_words = {i: line3}
+            print(all_words)
+
+
+
+
+finder2 = WordsFinder('test.txt')
+finder2.get_all_words()
